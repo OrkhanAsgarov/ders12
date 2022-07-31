47ed767a39ed6cdcd86618561f6b6e8c83126fc2
OrkhanAsgarov <105534845+OrkhanAsgarov@users.noreply.github.com>
2022-07-31 17:02:05 +0300

    react css kullanim orneklerine bakildi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import C1 from './components/C1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-  App / >
+root.render(
+  <C1 />
 );
